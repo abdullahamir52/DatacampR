78a983cde46aace55b79f64fbb84c985b87fcdcd
abdullahamir52 <abdullahamir52@gmail.com>
2020-09-08 02:41:21 +0600

    Done with introduction to R

--- Introduction to R/Chapter 1 Intro to basics.R
+++ Introduction to R/Chapter 1 Intro to basics.R
@@ -1,7 +1,11 @@
 # ==============================================================================
 ### Intro to basics
 # ==============================================================================
-  
+
+# Take your first steps with R. In this chapter, you will learn how to use the 
+# console as a calculator and how to assign variables. You will also get to 
+# know the basic data types in R. Let's get started.
+
 # ==============================================================================
 ## Arithmetic with R
 # ==============================================================================
--- Introduction to R/Chapter 3 Matrices.r
+++ Introduction to R/Chapter 3 Matrices.r
@@ -2,6 +2,12 @@
 ### Matrices
 # ==============================================================================
 
+# In this chapter, you will learn how to work with matrices in R. By the end of 
+# the chapter, you will be able to create matrices and understand how to do 
+# basic computations with them. You will analyze the box office numbers of 
+# the Star Wars movies and learn how to use matrices in R. May the force be 
+# with you!
+
 # ==============================================================================
 ## What's a matrix?
 # ==============================================================================
